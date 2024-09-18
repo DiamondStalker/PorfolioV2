67a4c3dfec038f2a3fe9a6eb2de154175413890b
DiamondStalker <cmoreno981@gmail.com>
2024-09-18 01:38:46 -0500

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 //import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import GitHubRepos from './components/Projects';
+// import GitHubRepos from './components/Projects';
 import Skills from './components/Skills'
 import Test from './components/Test'
 import './css/App.css'
+import './css/app.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
